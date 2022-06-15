26ef74ed8e59c6721c884a5af4f95e89c619c9ef
Jamsen <yiann.sky@gmail.com>
2022-06-15 16:34:45 +0200

    finish drop-file input

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,16 +14,80 @@ jQuery(document).ready(function ($) {
 });
 
 // Preloader
-const preloader = document.querySelector('.preloader');
+const preloader = document.querySelector(".preloader");
 
-preloader.classList.add('show-preloader');
+preloader.classList.add("show-preloader");
 
-window.addEventListener('load', function () {
-    	preloader.classList.remove('show-preloader');
+window.addEventListener("load", function () {
+  preloader.classList.remove("show-preloader");
 });
 
 // File input
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+  const dropZoneElement = inputElement.closest(".drop-zone");
 
+  dropZoneElement.addEventListener("click", (e) => {
+    inputElement.click();
+  });
+
+  const buttonBrowse = document.querySelector(".file-input-button");
+
+  buttonBrowse.addEventListener("click", (e) => {
+    inputElement.click();
+  });
+
+  inputElement.addEventListener("change", (e) => {
+    if (inputElement.files.length) {
+      updateThumbnail(dropZoneElement, inputElement.files[0]);
+    }
+  });
+
+  dropZoneElement.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropZoneElement.classList.add("drop-zone--over");
+  });
+
+  ["dragleave", "dragend"].forEach((type) => {
+    dropZoneElement.addEventListener(type, (e) => {
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+
+  dropZoneElement.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    if (e.dataTransfer.files.length) {
+      inputElement.files = e.dataTransfer.files;
+      updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+    }
+
+    dropZoneElement.classList.remove("drop-zone--over");
+  });
+});
+
+/**
+ * Updates the thumbnail on a drop zone element.
+ *
+ * @param {HTMLElement} dropZoneElement
+ * @param {File} file
+ */
+function updateThumbnail(dropZoneElement, file) {
+  let thumbnailElement = dropZoneElement.querySelector(".drop-zone__file-name");
+
+  // First time - remove the prompt
+  if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+    dropZoneElement.querySelector(".drop-zone__prompt").remove();
+  }
+
+  // First time - there is no thumbnail element, so lets create it
+  if (!thumbnailElement) {
+    thumbnailElement = document.createElement("span");
+    thumbnailElement.classList.add("drop-zone__file-name");
+    dropZoneElement.appendChild(thumbnailElement);
+  }
+
+  thumbnailElement.dataset.label = file.name;
+}
 
 // Boost calculate
 let boostResult = document.querySelector(".boost__counter--sum");
@@ -51,17 +115,6 @@ function titleInput(title) {
 const eleTitle = document.getElementById("title-form__input-title");
 
 eleTitle.addEventListener("keydown", function (e) {
-  const keyCode = e.which || e.keyCode;
-
-  if (keyCode === 13 && !e.shiftKey) {
-    e.preventDefault();
-  }
-});
-
-// Block enter on textarea subtitle
-const eleSubtitle = document.getElementById("title-form__input-subtitle");
-
-eleSubtitle.addEventListener("keydown", function (e) {
   const keyCode = e.which || e.keyCode;
 
   if (keyCode === 13 && !e.shiftKey) {
