5ba668d36b01139d84db5339e3cd508906066e9e
Jamsen <yiann.sky@gmail.com>
2022-06-15 13:22:03 +0200

    finish upload button

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,44 +24,6 @@ window.addEventListener('load', function () {
 
 // File input
 
-// dragover and dragenter events need to have 'preventDefault' called
-// in order for the 'drop' event to register. 
-// See: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Drag_operations#droptargets
-dropContainer.ondragover = dropContainer.ondragenter = function(evt) {
-  evt.preventDefault();
-};
-
-dropContainer.ondrop = function(evt) {
-  // pretty simple -- but not for IE :(
-  fileInput.files = evt.dataTransfer.files;
-
-  // If you want to use some of the dropped files
-  const dT = new DataTransfer();
-  dT.items.add(evt.dataTransfer.files[0]);
-  dT.items.add(evt.dataTransfer.files[3]);
-  fileInput.files = dT.files;
-
-  evt.preventDefault();
-};
-
-
-
-//TODO: Delete before deploy -->
-// Episodes dropdown list
-function show_list() {
-  var episodes = document.getElementById("episodes_id");
-
-  if (episodes.style.display == "block") {
-    episodes.style.display = "none";
-  } else {
-    episodes.style.display = "block";
-  }
-}
-window.onclick = function (event) {
-  if (!event.target.matches(".dropdown_button")) {
-    document.getElementById("episodes_id").style.display = "none";
-  }
-};
 
 // Boost calculate
 let boostResult = document.querySelector(".boost__counter--sum");
